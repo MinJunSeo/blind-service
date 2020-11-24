d7f83636a13a48a4d7b6e73432b421f2425bc3ac
MinJunSeo <smj0385@naver.com>
2020-11-24 16:01:29 +0900

    fix: getBusStop method

--- src/services/busStop.js
+++ src/services/busStop.js
@@ -3,24 +3,36 @@ const { apiKey } = require('../config');
 const xml2js = require('xml2js');
 
 class BusStop {
-  async getBusStop(xPos, yPos) {
-    const busStop = null;
+  async getBusStop(xpos, ypos) {
+    const busStop = [];
 
     const url = 'http://openapi.tago.go.kr/openapi/service/BusSttnInfoInqireService/getCrdntPrxmtSttnList';
     let queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + apiKey; /* Service Key*/
-    queryParams += '&' + encodeURIComponent('gpsLati') + '=' + encodeURIComponent(yPos); /* */
-    queryParams += '&' + encodeURIComponent('gpsLong') + '=' + encodeURIComponent(xPos); /* */
+    queryParams += '&' + encodeURIComponent('gpsLati') + '=' + encodeURIComponent(xpos); /* */
+    queryParams += '&' + encodeURIComponent('gpsLong') + '=' + encodeURIComponent(ypos); /* */
 
     request({
       url: url + queryParams,
       method: 'GET'
     }, (error, response, body) => {
-      console.log('Status', response.statusCode);
-      console.log('Headers', JSON.stringify(response.headers));
-      console.log('Reponse received', body);
-    });
+      xml2js.parseString(body, (err, result) => {
+        const arr = result.response.body[0].items[0].item;
+        for (var item of arr) {
+          const obj = {};
+          obj.cityCode = item.citycode[0];
+          obj.nodeId = item.nodeid[0];
+          obj.nodeNo = item.nodeno[0];
 
-    return busStop;
+          const distance = Math.acos(Math.sin(xpos) * Math.sin(item.gpslati[0]) + Math.cos(xpos) * Math.cos(item.gpslati[0]) * Math.cos(ypos - item.gpslong[0]));
+          obj.distance = distance;
+
+          busStop.push(obj);
+        }
+      });
+
+      console.log(busStop);
+      return busStop;
+    });
   }
 
   async getBusArrivalList(cityCode, nodeId) {
