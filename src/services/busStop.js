d64d1e946c55694f094771db831e0e748319f2a1
user <namsic6460@gmail.com>
2020-11-24 10:33:30 +0900

    Basic Code

--- src/services/busStop.js
+++ src/services/busStop.js
@@ -1,11 +1,44 @@
+var request = require('request');
+
 class BusStop {
   async getBusStop(xPos, yPos) {
     const busStop = null;
+
+    var url = 'http://ws.bus.go.kr/api/rest/stationinfo/getStationByPos';
+    var queryParams = '?' + encodeURIComponent('ServiceKey') + '=f%2BgH2HxZMaIPz54Y%2BU6j7qlKPAhzvBMP1%2FxNEFdh6EedEDuHMIDrXbw27Cp3Cz%2BRBeGEoTyPBu4dbbuQ7GoIvg%3D%3D'; /* Service Key*/
+    queryParams += '&' + encodeURIComponent('tmX') + '=' + encodeURIComponent(xPos); /* */
+    queryParams += '&' + encodeURIComponent('tmY') + '=' + encodeURIComponent(yPos); /* */
+    queryParams += '&' + encodeURIComponent('radius') + '=' + encodeURIComponent(100); /* */
+
+    request({
+      url: url + queryParams,
+      method: 'GET'
+    }, function (error, response, body) {
+      console.log('Status', response.statusCode);
+      console.log('Headers', JSON.stringify(response.headers));
+      console.log('Reponse received', body);
+    });
+
     return busStop;
   }
 
   async getBusArrivalList(stationId) {
     const busArrivalList = null;
+
+    var url = 'http://openapi.gbis.go.kr/ws/rest/busarrivalservice/station';
+    var queryParams = '?' + encodeURIComponent('ServiceKey') + '=f%2BgH2HxZMaIPz54Y%2BU6j7qlKPAhzvBMP1%2FxNEFdh6EedEDuHMIDrXbw27Cp3Cz%2BRBeGEoTyPBu4dbbuQ7GoIvg%3D%3D';
+    queryParams += '&' + encodeURIComponent('serviceKey') + '=' + encodeURIComponent('1234567890');
+    queryParams += '&' + encodeURIComponent('stationId') + '=' + encodeURIComponent('200000078');
+
+    request({
+      url: url + queryParams,
+      method: 'GET'
+    }, function (error, response, body) {
+      console.log('Status', response.statusCode);
+      console.log('Headers', JSON.stringify(response.headers));
+      console.log('Reponse received', body);
+    });
+
     return busArrivalList;
   }
 }
