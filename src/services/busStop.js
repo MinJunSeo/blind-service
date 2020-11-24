44a4e2f4117727dfba62683ae3c9ae5f74a6e6cf
MinJunSeo <smj0385@naver.com>
2020-11-24 10:19:51 +0900

    feat: BusStop getter method

---
+++ src/services/busStop.js
@@ -0,0 +1,13 @@
+class BusStop {
+  async getBusStop(xPos, yPos) {
+    const busStop = null;
+    return busStop;
+  }
+
+  async getBusArrivalList(stationId) {
+    const busArrivalList = null;
+    return busArrivalList;
+  }
+}
+
+module.exports = BusStop;
